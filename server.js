4f374ac45b7bc638773804f0ae12dcda5d5a648c
Arshaq shakeel <codeavenue.frontend03@gmail.com>
2020-12-18 19:15:43 +0500

    feat(Get/POST REQ): * Change mock array name arr to todos * Simple GEt todos endpoint and test with postman * Simple Post todos enpoint and it with postman
    Feat/master

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+
 const http = require("http");
 
-const arr = [
+const todos = [
   { id: 1, name: "Arshaq" },
   { id: 2, name: "Zubair" },
   { id: 3, name: "Hunain" },
 ];
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
+  const { method, url } = req;
   // console.log(headers, url, method, "Request");
-  res.setHeader("Content-Type", "application/json");
+  // res.setHeader("Content-Type", "application/json");
   // res.write("<h1>Hello World</h1>");
   // res.write();
   console.log(req.headers.authorization, "token here");
@@ -19,14 +20,32 @@ const server = http.createServer((req, res) => {
     })
     .on("end", () => {
       body = Buffer.concat(body).toString();
-      console.log(body);
+      // console.log(body);
+      let status = 400;
+      const response = {
+        success: false,
+        data: null,
+        message: ` ${status} Bad Request`,
+      };
+
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+        response.message = "Get Todos Success";
+      } else if (method === "POST" && url === "/todos") {
+        const { id, name } = JSON.parse(body);
+        todos.push({ id, name });
+        status = 201;
+        response.success = true;
+        response.data = todos;
+        response.message = `Todos Ceated Successfully`;
+      }
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify(response));
     });
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: arr,
-    })
-  );
   //   req.emit("")
 });
 
