4c553e88e17e329f29505d63d1dc008e493fe400
Arshaq shakeel <codeavenue.frontend03@gmail.com>
2020-12-20 20:49:42 +0500

    feat(controllers): * Create controllers for bootcamps
    Feat/master

---
+++ controllers/bootcamps.js
@@ -0,0 +1,58 @@
+const todos = require("../mock");
+
+// @desc Get All bootcamps
+// @route GET /api/v1/bootcamps
+// @access Public
+exports.getBootcamps = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, data: todos, message: "Show all bootcamps" });
+};
+
+// @desc Get Single bootcamps
+// @route GET /api/v1/bootcamps/:id
+// @access Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({
+    sucess: true,
+    data: todos.filter((item) => item.id === Number(req.params.id)),
+    message: `Get single bootcamp record`,
+  });
+};
+
+// @desc Post  bootcamps
+// @route POST /api/v1/bootcamps/:id
+// @access Public
+exports.createBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, data: todos, message: "Bootcamp created" });
+};
+
+// @desc update  bootcamps
+// @route PUT /api/v1/bootcamps/:id
+// @access Public
+exports.updateBootcamp = (req, res, next) => {
+  //   todos.push({ name: req.params.name });
+
+//   let updatedTodo = todos.map((item) => {
+//     item.id == req.params.id;
+//     // console.log()
+//     item.name = req.params.name;
+
+//   });
+  res.status(200).json({
+    sucess: true,
+    data: updatedTodo,
+    message: `Bootcamp Updated of ${req.params.id}`,
+  });
+};
+
+// @desc update  bootcamps
+// @route PUT /api/v1/bootcamps/:id
+// @access Public
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, data: todos, message: "Bootcamp has been deleted" });
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,45 +1,17 @@
 const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 const router = express.Router();
-const todos = require("../mock")
 
 // Learn Creating Basic routes
-router.get("/", (req, res) => {
-  //res.json(todos);
-  res
-    .status(200)
-    .json({ sucess: true, data: todos, message: "Show all bootcamps" });
-});
 
-router.get("/:id", (req, res) => {
-  //res.json(todos);
-  res.status(200).json({
-    sucess: true,
-    data: todos.filter((item) => item.id === Number(req.params.id)),
-    message: `Get single bootcamp record`,
-  });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-router.post("/", (req, res) => {
-  //res.json(todos);
-  res
-    .status(200)
-    .json({ sucess: true, data: todos, message: "Bootcamp created" });
-});
-
-router.put("/:id", (req, res) => {
-  //res.json(todos);
-  res.status(200).json({
-    sucess: true,
-    data: todos,
-    message: `Bootcamp Updated of ${req.params.id}`,
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  //res.json(todos);
-  res
-    .status(200)
-    .json({ sucess: true, data: todos, message: "Bootcamp has been deleted" });
-});
+router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 module.exports = router;
